2bc98ef8c0b0448ec4349fd63bbb2348b48ff0e8
oussama166 <oussamaouardi80@gmail.com>
2023-02-17 18:37:38 +0100

    End of project

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,99 @@
-// Verifaction of value and make error message 
-const errors = document.querySelector('.error')
-const price = document.querySelector('#price')
+const errors = document.querySelectorAll('.error')
+const prices = document.querySelectorAll('#price')
+const percents = document.querySelectorAll('.percent')
+const resultAmount = document.querySelector('.resultAmount')
+const resultTotal = document.querySelector('.resultTotal')
+const form = document.querySelector('button[type="reset"]')
+let bills = 0
+let persone = 1
+let tip = 1
 
-const checkNumber = (e)=>{
-  var reg = new RegExp('^[0-9]$');
-  var text = price.value
-  if(reg.test(e.key))
-    price.style.border = `1px solid green`
-  else 
-    console.log(text)
-    
+
+// Verifaction of value and make error message 
+const checkNumber = (e) => {
+  var reg = new RegExp('^[0-9.]$');
+  const data = e.data;
+  if (data) {
+    const isAllow = reg.test(data);
+    if (!isAllow) {
+      e.preventDefault();
+      e.target.classList.add('activeError')
+
+      setInterval(() => {
+        e.target.classList.remove('activeError')
+      }, 10000);
+      return e.target.dataset.info
+    }
+  }
 }
 
-price.addEventListener('keydown',checkNumber)
+const activeError = (info) => {
+  errors.forEach(error => {
+    if (info === error.dataset.type)
+      error.innerHTML = `data ivalid in ${info} field !!!`
+    setInterval(() => {
+      error.innerHTML = ``
+    }, 10000);
+    return
+  })
+}
+
+// Tip and Total calcule
+const calculeTip = () => {
+  let amout = (bills * tip) / persone
+  let total = (bills * tip)
+  amout = new Intl.NumberFormat('en-US',{
+    style: 'currency', currency: 'USD' 
+  }).format(amout)
+  total = new Intl.NumberFormat('en-US', {
+    style: 'currency', currency: 'USD'
+  }).format(total)
+  resultAmount.innerHTML = (amout === 'undefined') ? `$0.00` :amout 
+  resultTotal.innerHTML = (total === 'undefined') ? `$0.00` : total
+}
+
+prices.forEach(price => {
+  price.addEventListener('beforeinput', (e) => {
+    activeError(checkNumber(e))
+  })
+  price.addEventListener('keyup', () => {
+    prices.forEach(price => {
+      if (price.dataset.info === 'bills')
+        bills = parseFloat(price.value)
+      else if (price.dataset.info === 'membre')
+        persone = (price.value === 'NaN') ? 1 : parseInt(price.value)
+    })
+    calculeTip()
+  })
+})
+
+
+
+percents.forEach(percent => {
+  percent.addEventListener('click', (e) => {
+    percents.forEach(percent => {
+      percent.classList.remove('active')
+    });
+    e.target.classList.add('active');
+    if (e.target.classList.contains('custom')){
+      e.target.addEventListener('input',()=>{
+        tip = parseFloat((e.target.value)/100)
+        calculeTip()
+      })
+    }
+    else{
+        tip = parseFloat(e.target.dataset.percent)
+    }
+    calculeTip()
+    e.preventDefault()
+  })
+})
+
+
+
+
+
+
+
+
+
